29a889ff850c4e5b89691e00678cbd741f7b2645
Abhi <abhijadhav31122002@gmail.com>
2024-01-11 14:24:14 +0530

    Commit 4

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def home():
 
 @app.route('/predict_api',methods=["POST"])
 
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
@@ -20,6 +21,15 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
 
 if __name__=="main":
     app.run(debug=True)
